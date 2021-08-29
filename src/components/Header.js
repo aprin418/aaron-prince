f0b1382f12cddebc391a4a80dfbbba4cf48cb993
aprin418 <aprin418@gmail.com>
2021-08-29 16:22:04 -0400

    Added Header

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = () => {
+  return (
+    <div className="header-wraper">
+      <div className="main-info">
+        <h1>Aaron Prince, Software Engineer</h1>
+        <Typed
+          className="typed-text"
+          strings={["Web Development", "Software Engineering"]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+        <a href="#" className="btn-main-offer">
+          Contact Me
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
