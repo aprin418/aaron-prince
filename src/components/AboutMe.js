dacdcb86d1e14e69a7d37ea029d8f7c1f0cc9691
aprin418 <aprin418@gmail.com>
2021-09-01 15:37:48 -0400

    styling update

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,21 +3,23 @@ import author from "../Profile.jpg";
 
 const AboutMe = () => {
   return (
-    <div id="about" className="container py-5">
-      <div className="row">
-        <div className="col-lg-6 col-xm-12">
-          <div className="photo-wrap mb-5">
-            <img className="profile-img" src={author} alt="author photo" />
+    <div className="about">
+      <div id="about" className="container py-5">
+        <div className="row">
+          <div className="col-lg-6 col-xm-12">
+            <div className="photo-wrap mb-5">
+              <img className="profile-img" src={author} alt="author photo" />
+            </div>
           </div>
-        </div>
-        <div className="col-lg-6 col-xm-12">
-          <h1 className="about-heading">About Me</h1>
-          <p>
-            I am a software engineer with a history of problem solving, teamwork
-            and leadership. I value independence, responsibility and success. I
-            have a proven record of finding and fixing problems while working as
-            an automotive technician with BMW.
-          </p>
+          <div className="col-lg-6 col-xm-12">
+            <h1 className="about-heading">About Me</h1>
+            <p>
+              I am a software engineer with a history of problem solving,
+              teamwork and leadership. I value independence, responsibility and
+              success. I have a proven record of finding and fixing problems
+              while working as an automotive technician with BMW.
+            </p>
+          </div>
         </div>
       </div>
     </div>
